284085988d13eeedc3a9bab01181245ce1b53575
mswertz <m.a.swertz@rug.nl>
2019-04-26 22:48:44 +0200

    added resize window to have predictable resolution

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ fixture `Getting Started`
 
 test('My first test', async t => {
   await t
+    .resizeWindow(1280, 800)
     .click('#open-button')
     .typeText('#username-field', 'demo')
     .typeText('#password-field', 'demo')
