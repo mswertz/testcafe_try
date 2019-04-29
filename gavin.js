9b1c3a9bba9ed773e74e80b4701629f879dd2fbb
mswertz <m.a.swertz@rug.nl>
2019-04-29 23:41:47 +0200

    added gavin test

---
+++ gavin.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe';
+
+fixture `Getting Started`
+  .page `https://molgenis20.gcc.rug.nl`;
+
+test('Gavin test 1', async t => {
+  await t
+    .resizeWindow(1280, 800)
+    .click(Selector('.btn').withText('Upload file'))
+    .setFilesToUpload('input[type=file]', 'files/gavin_simpleexamplefile_truevcf.vcf')
+    .typeText('input[type=string]', 'blaat')
+    .click('#upload-file-btn')
+    .expect(Selector('a.btn.btn-info').withText('All done!').exists).ok({
+    timeout: 60000
+  });
+// Test code
+});
