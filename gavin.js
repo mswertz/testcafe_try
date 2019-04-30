84f3152881c9bc26d6c5744925addcbbf2535321
mswertz <m.a.swertz@rug.nl>
2019-04-30 13:48:22 +0200

    extended gavin test to also test contents of result

--- gavin.js
+++ gavin.js
@@ -1,9 +1,11 @@
 import { Selector } from 'testcafe';
+const fetch = require('node-fetch');
 
 fixture `Gavin smoketest`
   .page `https://molgenis20.gcc.rug.nl`;
 
 test('Gavin test 1', async t => {
+  //test if complete succesfull
   await t
     .resizeWindow(1280, 800)
     .click(Selector('.btn').withText('Upload file'))
@@ -12,6 +14,16 @@ test('Gavin test 1', async t => {
     .click('#upload-file-btn')
     .expect(Selector('a.btn.btn-info').withText('All done!').exists).ok({
     timeout: 60000
-  });
-// Test code
+  })
+
+  //test if content caontains expected values
+  let href = 'https://molgenis20.gcc.rug.nl' + await Selector('.btn-primary').withText('Download GAVIN').getAttribute('href');
+  console.log(href);
+
+  await t.expect(Selector('.btn-primary').withText('Download GAVIN').getAttribute('href')).contains('/plugin/gavin-app')
+    .expect(await fetch(href).then(
+      function(response) {
+        return response.text();
+      }
+    )).contains('CADD=-0.188424');
 });
